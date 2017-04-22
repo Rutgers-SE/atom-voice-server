43a54880ab2851811494163ba12c23a8111fcc35
Alexander Sanchez <asanchez.dev@gmail.com>
2017-04-22 16:48:14 -0400

    it kinda works

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const net = require('net');
 const PORT = 10001;
 const HOST = '127.0.0.1';
+var record = require('node-record-lpcm16');
+const fs = require('fs');
 
 
 // setup google
@@ -10,11 +12,16 @@ var speechClient = speech({
     keyFilename: './cloud-key.json'
 });
 
+const requestFile = {
+    encoding: 'LINEAR16',
+    sampleRateHertz: 16000,
+    languageCode: 'en-US'
+};
 
 const request = {
     config: {
         encoding: 'LINEAR16',
-        sampleRateHertz: 44100,
+        sampleRateHertz: 16000,
         languageCode: 'en-US'
     }
 };
@@ -22,17 +29,23 @@ const request = {
 const recognizeStream = speechClient.createRecognizeStream(request)
       .on('error', console.error)
       .on('data', (data) => {
-          console.log(`Google: ${data.results}`);
+          process.stdout.write(`Google: ${data.results}\n`);
       });
 
 
-net.createServer((s) => {
-    // s.on('data', (data) => {
-    //     console.log(data);
-    // });
-
+var n = net.createServer((s) => {
     s.pipe(recognizeStream);
-
+    s.on('close', () => {
+        console.log("Passing to Google");
+        // soundFile.pipe(recognizeStream);
+        // speechClient.recognize('./file.wav', request)
+        //     .then((results) => {
+        //         console.log(results[0]);
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //     });
+    });
 }).listen(PORT, HOST, () => {
     console.log("listening on ", PORT);
 });
